65ab469f5c1d165ec9c09b671b6fd4bc88d6fc97
Joey <joey@lineageos.org>
2018-02-04 20:41:06 +0100

    lite: add BUILD_RRO_SYSTEM_PACKAGE target
    Allows to ship rro packages in system instead of vendor
    
    Change-Id: I8822ca477e80297dce477de2d87bd3e74d4cbfb0
    Signed-off-by: Joey <joey@lineageos.org>

--- build/core/config.mk
+++ build/core/config.mk
@@ -3,3 +3,5 @@ include $(TOPDIR)vendor/lite/build/core/mtk_target.mk
 
 # Rules for QCOM targets
 include $(TOPDIR)vendor/lite/build/core/qcom_target.mk
+
+BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)/vendor/lite/build/core/system_rro.mk
