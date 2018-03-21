9777bd1d2c5ce584934ebe4a1c0bd8331e9e92e0
Michael Bestas <mkbestas@lineageos.org>
2018-03-21 18:29:45 +0200

    lite: Set TARGET_CRYPTFS_HW_PATH if HW crypto is enabled
    * So TWRP can use it
    
    Change-Id: Ibc1d9d943d6d4e4e5248f623cd06782335f8ae98

--- BoardConfigLite.mk
+++ BoardConfigLite.mk
@@ -5,3 +5,8 @@ ifeq ($(ODEX),true)
     WITH_DEXPREOPT := true
     DISABLE_DEXPREOPT := false
 endif
+
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
