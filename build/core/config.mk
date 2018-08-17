95cf9b9a49a155b38c6a48a6331f6a5d7947dc4c
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-17 09:20:21 -0700

    lite: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a

--- build/core/config.mk
+++ build/core/config.mk
@@ -5,3 +5,8 @@ include $(TOPDIR)vendor/lite/build/core/mtk_target.mk
 include $(TOPDIR)vendor/lite/build/core/qcom_target.mk
 
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)/vendor/lite/build/core/system_rro.mk
+
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
