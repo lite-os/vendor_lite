9e549594056a48e8aaccfbdab3f558d56d800c75
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    lineage: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- BoardConfigLite.mk
+++ BoardConfigLite.mk
@@ -6,7 +6,6 @@ ifeq ($(ODEX),true)
     DISABLE_DEXPREOPT := false
 endif
 
-# QCOM HW crypto
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/lite/config/BoardConfigQcom.mk
 endif
